69b182b35a29ad604084b5c74b2302402ac90e64
Biju Joseph <biju.joseph@semanticbits.com>
2016-09-23 15:24:05 -0400

    FDA-93 add sample apps

--- pages/community/index.jsx
+++ pages/community/index.jsx
@@ -16,16 +16,10 @@ export default () => (
             label='Community'
             title='openFDA Apps'
             htmlDescription="true"
-            description="Part of openFDA's mission is to make public FDA data quickly and easily accessible for use in applications. Members of the openFDA community have responded to that opportunity by creating new apps that further the reach of the available information and extend the benefits of the openFDA database. <br/> <br/> openFDA developers wishing to see their app appear on this page may send a request to <a href='mail-to:open@fda.hhs.gov' class='clr-white underline'>open@fda.hhs.gov</a>."
+            description="<b>FDA is not responsible for Section 508 compliance (accessibility) on other federal or private websites. See <a href='http://www.hhs.gov/disclaimer.html' class='clr-white underline' >HHS’s Website Disclaimer</a>.</b><br/><br/><b>FDA is not responsible for the contents of any pages referred from its website. See <a class='clr-white underline' href='http://www.fda.gov/AboutFDA/AboutThisWebsite/WebsitePolicies/#sites'>FDA’s Website Policy</a> and the <a class='clr-white underline' href='https://www.whitehouse.gov/sites/default/files/omb/assets/memoranda_2010/m10-23.pdf'>OMB's Guidance for Agency Use of Third-Party Websites and Applications</a>.</b><br/><br/>App developers may request to have their openFDA-related apps appear on this page by submitting an inquiry to <a href='mailto:open@fda.hhs.gov' class='clr-white underline'>open@fda.hhs.gov</a>."
         />
 
         <section className='container clearfix marg-t-3 marg-b-3 relative'>
-            <div className="usa-alert usa-alert-info">
-                <div className="usa-alert-body">
-                    <p className="usa-alert-text">FDA is not responsible for Section 508 compliance (accessibility) on other federal or private websites. See <a href="http://www.hhs.gov/disclaimer.html">HHS’s Website Disclaimer</a></p>
-                    <p className="usa-alert-text">FDA is not responsible for the contents of any pages referred from its website. See <a href="http://www.fda.gov/AboutFDA/AboutThisWebsite/WebsitePolicies/#sites">FDA’s Website Policy</a> and the <a href="https://www.whitehouse.gov/sites/default/files/omb/assets/memoranda_2010/m10-23.pdf">2010 Whitehouse Memorandum</a></p>
-                </div>
-            </div>
 
             <div className="flex-box dir-row flex-wrap">
                     {
