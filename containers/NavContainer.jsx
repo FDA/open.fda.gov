4512a3856ba29d03bfb433ebea6a777be09a35d6
beardedfinch <jack.finch@semanticbits.com>
2017-01-11 16:21:07 -0600

    fda-154 Updated mobile menu.

--- containers/NavContainer.jsx
+++ containers/NavContainer.jsx
@@ -4,17 +4,46 @@ import React from 'react'
 
 type tSTATE = {
   showMobileNav: boolean;
+  toggleDropdownContent: boolean;
+  activeDropdown: string;
 };
 
 const NavContainer = function (ComposedNav: ReactClass): ReactClass {
   class HOC extends React.Component {
     state: tSTATE = {
-      showMobileNav: false
+      showMobileNav: false,
+      activeDropdown: ' '
     };
 
     _toggleMobileNav () {
       this.setState({
         showMobileNav: !this.state.showMobileNav,
+      })
+    }
+
+    _toggleDropdownContent (e) {
+      let title = e.target.getAttribute('title')
+      if (this.state.activeDropdown != title) {
+        this.setState({
+          activeDropdown: title
+        })
+      } else {
+        this.setState({
+          activeDropdown: ' '
+        })
+      }
+    }
+
+    _hideDropdownContent () {
+      this.setState({
+        activeDropdown: ' '
+      })
+    }
+
+    _showDropdownContent (e) {
+      let title = e.target.getAttribute('title')
+      this.setState({
+        activeDropdown: title
       })
     }
 
@@ -24,6 +53,9 @@ const NavContainer = function (ComposedNav: ReactClass): ReactClass {
           {...this.props}
           {...this.state}
           toggleMobileNav={this._toggleMobileNav.bind(this)}
+          toggleDropdownContent={this._toggleDropdownContent.bind(this)}
+          hideDropdownContent={this._hideDropdownContent.bind(this)}
+          showDropdownContent={this._showDropdownContent.bind(this)}
         />
       )
     }
