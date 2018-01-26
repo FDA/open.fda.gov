92ca18e191c116a9206f8da1026dda8af315555a
beardedfinch <jack.finch@semanticbits.com>
2018-01-26 10:41:34 -0600

    Fixed API Status page to include ADL.

--- components/ApiStatus.jsx
+++ components/ApiStatus.jsx
@@ -12,7 +12,7 @@ type tPROPS = {
 };
 
 const catMap: Object = Object.freeze({
-  'animaldruglabel': 'Animal & Veterinary › Labeling',
+  'animalandveterinarylabel': 'Animal & Veterinary › Labeling',
   'foodenforcement': 'Foods › Enforcement Reports',
   'foodevent': 'Foods › Adverse Events',
   'drugevent': 'Human Drugs › Adverse Events',
@@ -30,7 +30,7 @@ const catMap: Object = Object.freeze({
 
 
 const endpointLinkMap: Object = Object.freeze({
-  'animaldruglabel': 'animalandveterinary/label',
+  'animalandveterinarylabel': 'animalandveterinary/label',
   'drugevent': 'drug/event',
   'druglabel': 'drug/label',
   'foodenforcement': 'food/enforcement',
