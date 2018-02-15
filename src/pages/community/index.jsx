90d0bcc1bd35d8e77631a15c5d388bbb303bcca4
beardedfinch <beardedfinch@gmail.com>
2018-02-15 14:35:51 -0600

    Community App page mobile update.

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -15,7 +15,7 @@ export default () => (
       description="<b>FDA is not responsible for Section 508 compliance (accessibility) on other federal or private websites. See <a href='http://www.hhs.gov/disclaimer.html' class='clr-white underline' >HHS’s Website Disclaimer</a>.</b><br/><br/><b>FDA is not responsible for the contents of any pages referred from its website. See <a class='clr-white underline' href='http://www.fda.gov/AboutFDA/AboutThisWebsite/WebsitePolicies/#sites'>FDA’s Website Policy</a> and the <a class='clr-white underline' href='https://www.whitehouse.gov/sites/default/files/omb/assets/memoranda_2010/m10-23.pdf'>OMB's Guidance for Agency Use of Third-Party Websites and Applications</a>.</b><br/><br/>App developers may request to have their openFDA-related apps appear on this page by submitting an inquiry to <a href='mailto:open@fda.hhs.gov' class='clr-white underline'>open@fda.hhs.gov</a>."
     />
 
-    <section style={{width: '1140px', margin: 'auto'}}>
+    <section className='apps-section'>
       <div className='apps-container'>
         {
           tools.map((tool, i) => (
