9beff7b99fcef1ff60d42ad7961b82ca8383c722
beardedfinch <beardedfinch@gmail.com>
2018-02-15 14:19:48 -0600

    Community App page update.

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -15,22 +15,17 @@ export default () => (
       description="<b>FDA is not responsible for Section 508 compliance (accessibility) on other federal or private websites. See <a href='http://www.hhs.gov/disclaimer.html' class='clr-white underline' >HHS’s Website Disclaimer</a>.</b><br/><br/><b>FDA is not responsible for the contents of any pages referred from its website. See <a class='clr-white underline' href='http://www.fda.gov/AboutFDA/AboutThisWebsite/WebsitePolicies/#sites'>FDA’s Website Policy</a> and the <a class='clr-white underline' href='https://www.whitehouse.gov/sites/default/files/omb/assets/memoranda_2010/m10-23.pdf'>OMB's Guidance for Agency Use of Third-Party Websites and Applications</a>.</b><br/><br/>App developers may request to have their openFDA-related apps appear on this page by submitting an inquiry to <a href='mailto:open@fda.hhs.gov' class='clr-white underline'>open@fda.hhs.gov</a>."
     />
 
-    <section className='container clearfix marg-t-3 marg-b-3 relative'>
-      <div className="flex-box dir-row flex-wrap">
+    <section style={{width: '1140px', margin: 'auto'}}>
+      <div className='apps-container'>
         {
           tools.map((tool, i) => (
-            <div className="col d-3 b-t-1 grow-none" key={i}>
-              <div className="flex-box">
-                <h4>
-                  <a
-                    className={aCx}
-                    href={tool.url}
-                    target="_blank">{tool.title}
-                  </a>
-                  <a href={disclaimer} target='_blank' className='link-external'/>
-                </h4>
-              </div>
-            </div>
+            <a
+              key={i}
+              className={aCx}
+              href={tool.url}
+              target='_blank'>
+              {tool.title}
+            </a>
           ))
         }
       </div>
