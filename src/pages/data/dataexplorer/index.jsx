c312c7b5d74db2f229fa788af3f1c6fb4eb967be
John DeBovis <jdebovis@netdes.com>
2018-02-19 10:41:36 -0500

    fix dataexplorer page

--- src/pages/data/dataexplorer/index.jsx
+++ src/pages/data/dataexplorer/index.jsx
@@ -6,27 +6,72 @@ import meta from './_meta.yaml'
 import datasets from './_datasets.yaml'
 import Select from 'react-select'
 
+class DataExplorer extends React.Component {
 
-export default () => (
-  <section>
-    <Hero
-      {...meta}
-    />
-    <section className='body-bg-offwhite'>
-      <div className='container blog-bg'>
-        <div className='blog-container'>
-          <div style={{
-            height: 100
-          }}>
-          {"I'm Interested In: "}
-          <Select
-            name="toggle"
-            options={datasets.names}
-            placeholder="Search the fields"
-          />
+   constructor (props: Object) {
+    super(props)
+
+    const options = Object.keys(datasets.names).map(name => {
+      return datasets.names[name]
+    })
+
+    this.state = {
+      options: options,
+      choosenDataset: options[0]
+    }
+  }
+  componentWillReceiveProps(){
+    
+  }
+  componentDidMount () {
+    
+  }
+
+  render (): ?React.Element {
+
+    return (
+      <section>
+        <Hero
+          {...meta}
+        />
+        <section className='body-bg-offwhite'>
+          <div className='container blog-bg'>
+            <div className='blog-container'>
+              <div style={{
+                height: 100,
+                width: 1000,
+                display:"flex",
+                paddingTop: 20
+              }}>
+              <i style={{
+                fontSize: 22,
+                paddingTop: 6,
+                paddingRight: 5
+              }}>I'm Interested In:</i>
+              <Select
+                style={{
+                  height: 30,
+                  width: 500
+                }}
+                clearable={false}
+                resetValue="label"
+                value={this.state.choosenDataset}
+                name="toggle"
+                options={this.state.options}
+                placeholder="Search the fields"
+              />
+              </div>
+              <div>
+                
+
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
-  </section>
-)
+        </section>
+      </section>
+    )
+  }
+}
+
+
+export default DataExplorer
