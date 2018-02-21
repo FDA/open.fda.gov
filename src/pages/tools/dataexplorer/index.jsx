a6d6ac9fcd2f6c5503fa64b1a3f5d00b0c880d32
gundalar <ramakrishna.gundala@semanticbits.com>
2018-02-21 13:15:17 -0500

    add API specific Particularly select box, add example autosuggest

--- src/pages/tools/dataexplorer/index.jsx
+++ src/pages/tools/dataexplorer/index.jsx
@@ -9,24 +9,67 @@ import Select from 'react-select'
 
 class DataExplorer extends React.Component {
 
-   constructor (props: Object) {
+  constructor (props: Object) {
     super(props)
 
     const options = Object.keys(datasets.names).map(name => {
+      const viewsArray = Object.assign({}, datasets.names[name].views)
+      datasets.names[name].views = []
+      Object.keys(viewsArray).map(function (k) {
+        datasets.names[name].views.push(viewsArray[k])
+      })
+
       return datasets.names[name]
     })
 
     this.state = {
       options: options,
-      choosenDataset: options[0]
+      choosenDataset: options[0],
+      view: options[0].views[0]
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleViewChange = this.handleViewChange.bind(this)
+
   }
-  componentWillReceiveProps(){
-    
+  componentWillReceiveProps () {
+
   }
   componentDidMount () {
-    
+    this.handleChange(this.state.choosenDataset)
+    this.handleViewChange(this.state.view)
+
   }
+
+  handleChange (value) {
+    let choice = null
+    this.state.options.forEach(obj => {
+      if (obj.label == value.label) {
+        choice = obj
+        console.log("choice is:" + choice.label)
+      }
+    })
+
+    this.setState({
+      choosenDataset: choice,
+      view: choice.views[0]
+    })
+  }
+
+  handleViewChange (value) {
+    let choice = null
+    this.state.choosenDataset.views.forEach(obj => {
+      if (obj.label == value.label) {
+        choice = obj
+        console.log("choice is:" + choice.label)
+      }
+    })
+
+    this.setState({
+      view: choice
+    })
+  }
+
 
   render (): ?React.Element {
 
@@ -44,29 +87,49 @@ class DataExplorer extends React.Component {
               <div style={{
                 height: 70,
                 width: 1000,
-                display:"flex",
+                display: "flex",
                 paddingTop: 20,
                 width: "100%",
                 borderBottom: 1,
                 borderBottomStyle: "solid"
               }}>
-              <i style={{
-                fontSize: 22,
-                paddingTop: 6,
-                paddingRight: 5
-              }}>I'm Interested In:</i>
-              <Select
-                style={{
-                  height: 30,
-                  width: 500
-                }}
-                clearable={false}
-                resetValue="label"
-                value={this.state.choosenDataset}
-                name="toggle"
-                options={this.state.options}
-                placeholder="Search the fields"
-              />
+                <i style={{
+                  fontSize: 22,
+                  paddingTop: 6,
+                  paddingRight: 5
+                }}>I'm Interested In:</i>
+                <Select
+                  style={{
+                    height: 30,
+                    width: 300
+                  }}
+                  clearable={false}
+                  resetValue='label'
+                  value={this.state.choosenDataset}
+                  name='toggle'
+                  options={this.state.options}
+                  onChange={this.handleChange}
+                  placeholder='Search the fields'
+                />
+                <i style={{
+                  fontSize: 22,
+                  paddingTop: 6,
+                  paddingRight: 5,
+                  marginLeft: 200,
+                }}>Particularly:</i>
+                <Select
+                  style={{
+                    height: 30,
+                    width: 300
+                  }}
+                  clearable={false}
+                  resetValue='label'
+                  value={this.state.view}
+                  name='toggle'
+                  options={this.state.choosenDataset.views}
+                  onChange={this.handleViewChange}
+                  placeholder='Select view'
+                />
               </div>
               <FilterComponent
                 dataset={this.state.choosenDataset}
@@ -81,13 +144,5 @@ class DataExplorer extends React.Component {
 
 
 export default DataExplorer
-
-
-
-
-
-
-
-
 
 
