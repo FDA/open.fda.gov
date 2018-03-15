ffcbc12d1e5591b76f5f9c32866007548d42e794
beardedfinch <beardedfinch@gmail.com>
2018-03-15 14:29:21 -0500

    Added more product_type options.

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,4 +1,4 @@
-const API_LINK: string = "https://api.fda.gov"
+const API_LINK: string = "http://ec2-34-238-202-4.compute-1.amazonaws.com:8000"
 const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
