b6075c75c3a421977163154b330a23406a3ad808
beardedfinch <beardedfinch@gmail.com>
2018-06-29 10:52:14 -0500

    FDA-258 Added Phish-Pharm endpoint pages.

--- src/pages/apis/animalandveterinary/index.jsx
+++ src/pages/apis/animalandveterinary/index.jsx
@@ -19,6 +19,10 @@ export default () => (
           noun_name='animalandveterinary'
           endpoint_name='petfood_reports'
         />
+        <EndpointBox
+          noun_name='animalandveterinary'
+          endpoint_name='phishpharm'
+        />
       </div>
     </div>
   </section>
--- src/pages/apis/animalandveterinary/phishpharm/example-api-queries/index.jsx
+++ src/pages/apis/animalandveterinary/phishpharm/example-api-queries/index.jsx
@@ -7,12 +7,11 @@ class IndexRoute extends React.Component {
   render() {
 
     const oneRecord = explorers['oneRecord']
-    const countDeadlyProducts = explorers['countDeadlyProducts']
-    const femaleChihuahuas = explorers['femaleChihuahuas']
+    const countBySpecies = explorers['countBySpecies']
 
     return (
       <section className="doc-content">
-        <h2>Example pet food API queries</h2>
+        <h2>Example Phish-Pharm API queries</h2>
         <p>To help get you started, we have provided some API query examples below. Use the Run query button to call the API and get back results. You can experiment by editing the example queries in the black text box.</p>
         <QueryExplorer
           desc={oneRecord.description}
@@ -21,16 +20,10 @@ class IndexRoute extends React.Component {
           title={oneRecord.title}
         />
         <QueryExplorer
-          desc={countDeadlyProducts.description}
-          originalQuery={countDeadlyProducts.query}
-          params={countDeadlyProducts.params}
-          title={countDeadlyProducts.title}
-        />
-        <QueryExplorer
-          desc={femaleChihuahuas.description}
-          originalQuery={femaleChihuahuas.query}
-          params={femaleChihuahuas.params}
-          title={femaleChihuahuas.title}
+          desc={countBySpecies.description}
+          originalQuery={countBySpecies.query}
+          params={countBySpecies.params}
+          title={countBySpecies.title}
         />
       </section>
     )
