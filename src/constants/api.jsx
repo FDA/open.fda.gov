0bb72ffcbed3fedceb323d2a5d32641ce6d2818f
beardedfinch <beardedfinch@gmail.com>
2018-09-10 13:30:01 -0500

    FDA-371 Updated api links.

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,4 +1,4 @@
-const API_LINK: string = "https://api.openfda-adae-demo.semanticbits.com"
+const API_LINK: string = "https://openfda-api.preprod.fda.gov"
 const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
