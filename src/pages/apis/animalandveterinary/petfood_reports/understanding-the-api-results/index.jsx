1c2e2c19c9f5c880230f1937e3f34197267fced2
beardedfinch <beardedfinch@gmail.com>
2018-09-11 10:53:13 -0500

    Pet Food demo prep

--- src/pages/apis/animalandveterinary/petfood_reports/understanding-the-api-results/index.jsx
+++ src/pages/apis/animalandveterinary/petfood_reports/understanding-the-api-results/index.jsx
@@ -10,14 +10,14 @@ class IndexRoute extends React.Component {
     return (
       <section className="doc-content">
         <h2>Understanding the API Results</h2>
-        <p>For <code>search</code> queries (such as: <a href="https://api.fda.gov/animalandveterinary/petfood_reports.json?search=report_year:2015&limit=1">https://api.fda.gov/animalandveterinary/petfood_reports.json?search=report_year:2015&limit=1</a>), the <code>results</code> section includes matching pet food reports returned by the API.</p>
+        <p>For <code>search</code> queries (such as: <a href="https://openfda-api.preprod.fda.gov/animalandveterinary/petfood_reports.json?search=report_year:2015&limit=1">https://openfda-api.preprod.fda.gov/animalandveterinary/petfood_reports.json?search=report_year:2015&limit=1</a>), the <code>results</code> section includes matching pet food reports returned by the API.</p>
         <p>Each pet food report consists of these major sections:</p>
         <ul>
           <li>Product Information, detailing the product referred to in the pet food record.</li>
           <li>Animal Information, identifying the animals affected and their traits.</li>
           <li>Adverse effect information.</li>
         </ul>
-        <p>For <code>count</code> queries (such as: <a href="https://api.fda.gov/animalandveterinary/petfood_reports.json?count=product.formulation">https://api.fda.gov/animalandveterinary/petfood_reports.json?count=product.formulation</a>), the results section will look something like the following:</p>
+        <p>For <code>count</code> queries (such as: <a href="https://openfda-api.preprod.fda.gov/animalandveterinary/petfood_reports.json?count=product.formulation.exact">https://openfda-api.preprod.fda.gov/animalandveterinary/petfood_reports.json?count=product.formulation.exact</a>), the results section will look something like the following:</p>
         <Highlight
           className='javascript'>
           {example}
