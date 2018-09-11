beb5c9df50e4eaac8dce36eca0b337fc031ff1c6
beardedfinch <beardedfinch@gmail.com>
2018-09-11 10:25:50 -0500

    FDA-371 Updated api links.

--- src/pages/apis/animalandveterinary/petfood_reports/explore-the-api-with-an-interactive-chart/index.jsx
+++ src/pages/apis/animalandveterinary/petfood_reports/explore-the-api-with-an-interactive-chart/index.jsx
@@ -21,7 +21,7 @@ class IndexRoute extends React.Component {
           fieldsFlattened={fieldsFlattened}
           fields={fields}
           infographics={infographics}
-          api='https://api.openfda-petfoods.semanticbits.com'
+          api='https://openfda-api.preprod.fda.gov'
           meta={meta}
         />
       </section>
