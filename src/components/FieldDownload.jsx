e82216d8394ed884d5e3000873c991f6684da319
beardedfinch <beardedfinch@gmail.com>
2018-09-11 14:59:50 -0500

    Pet food demo prep

--- src/components/FieldDownload.jsx
+++ src/components/FieldDownload.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const pdfDownloadMap: Object = Object.freeze({
   'animalandveterinaryevent': '/fields/animalandveterinaryevent_reference.pdf',
-  'animalandveterinarypetfood': '/fields/animalandveterinarypetfood_reference.pdf',
+  'animalandveterinarypetfood_reports': '/fields/animalandveterinarypetfood_reference.pdf',
   'foodevent': '/fields/foodevent_reference.pdf',
   'foodenforcement': '/fields/foodenforcement_reference.pdf',
   'drugevent': '/fields/drugevent_reference.pdf',
@@ -20,7 +20,7 @@ const pdfDownloadMap: Object = Object.freeze({
 
 const xlsxDownloadMap: Object = Object.freeze({
   'animalandveterinaryevent': '/fields/animalandveterinaryevent_reference.xlsx',
-  'animalandveterinarypetfood': '/fields/animalandveterinarypetfood_reference.xlsx',
+  'animalandveterinarypetfood_reports': '/fields/animalandveterinarypetfood_reference.xlsx',
   'foodevent': '/fields/foodevent_reference.xlsx',
   'foodenforcement': '/fields/foodenforcement_reference.xlsx',
   'drugevent': '/fields/drugevent_reference.xlsx',
@@ -38,7 +38,7 @@ const xlsxDownloadMap: Object = Object.freeze({
 
 const yamlDownloadMap: Object = Object.freeze({
   'animalandveterinaryevent': '/fields/animalandveterinaryevent.yaml',
-  'animalandveterinarypetfood': '/fields/animalandveterinarypetfood_.yaml',
+  'animalandveterinarypetfood_reports': '/fields/animalandveterinarypetfood.yaml',
   'foodevent': '/fields/foodevent.yaml',
   'foodenforcement': '/fields/foodenforcement.yaml',
   'drugevent': '/fields/drugevent.yaml',
