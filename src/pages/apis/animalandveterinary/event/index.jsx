54e4c179d74683a61f83739fdb4697b9b38c4fba
beardedfinch <beardedfinch@gmail.com>
2018-09-18 12:38:02 -0500

    ADAE text tweaks

--- src/pages/apis/animalandveterinary/event/index.jsx
+++ src/pages/apis/animalandveterinary/event/index.jsx
@@ -23,6 +23,11 @@ class IndexRoute extends React.Component {
         <p>Do not rely on openFDA to make decisions regarding medical care. Always speak to your health provider about the risks and benefits of FDA-regulated products. We may limit or otherwise restrict your access to the API in line with our <Link to="/terms/">Terms of Service</Link>.</p>
         <p>Adverse event reports submitted to FDA do not undergo extensive validation or verification. Therefore, <strong>a causal relationship cannot be established between product and reactions listed in a report.</strong> While a suspected relationship may exist, it is not medically validated and should not be the sole source of information for clinical decision making or other assumptions about the safety or effectiveness of a product.</p>
         <p>Additionally, it is important to remember that adverse event reports represent a small percentage of total usage numbers of a product. Commonly used products may have a higher number of reported adverse events due to the higher total number of animals being given the product.</p>
+
+        <h3>Disclaimer</h3>
+        <p>Animal drug AE data does have limitations. There is no certainty that the reported event (adverse event or medication error) was actually due to the product. FDA does not require that a causal relationship between a product and event be proven, and reports do not always contain enough detail to properly evaluate an event.</p>
+        <p>Further, FDA does not receive reports for every adverse event or medication error that occurs with a product. Many factors can influence whether or not an event will be reported, such as the time a product has been marketed and publicity about an event.</p>
+        <p>Submission of a safety report does not constitute an admission that medical personnel, user facility, importer, distributor, manufacturer or product caused or contributed to the event. The information in these reports has not been scientifically or otherwise verified as to a cause and effect relationship and cannot be used to estimate the incidence of these events.</p>
       </section>
     )
   }
