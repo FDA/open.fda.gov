558407a9c12079e8c07f67751cd948066d1f278f
beardedfinch <beardedfinch@gmail.com>
2018-11-21 10:01:56 -0600

    Removed placeholder text for Lonnie.

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -42,10 +42,10 @@ const Disclaimer = (props: tPROPS) => {
       <h4 className="modal-header">Disclaimer</h4>
       <div className='modal-body'>
         <p>Do not rely on openFDA to make decisions regarding medical care. Always speak to your health provider about the risks and benefits of FDA-regulated products.  We may limit or otherwise restrict your access to the API in line with our <Link className='underline' to='/terms/'> Terms of Service</Link></p>
-        {
-          path === '/apis/drug/ndc/' &&
-          <p>NDC disclaimer text placeholder.</p>
-        }
+        {/*{*/}
+          {/*path === '/apis/drug/ndc/' &&*/}
+          {/*<p>NDC disclaimer text placeholder.</p>*/}
+        {/*}*/}
       </div>
       <button className='button bg-primary clr-white' onClick={() => handleCloseModal(hideModal)}>ACCEPT</button>
     </ReactModal>
