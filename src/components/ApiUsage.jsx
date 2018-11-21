078b9b6fe2efa5dabf1162cac7bbda791a708fab
beardedfinch <beardedfinch@gmail.com>
2018-11-21 10:07:55 -0600

    Updated since launch usage.

--- src/components/ApiUsage.jsx
+++ src/components/ApiUsage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { TimeSeries, TimeRange} from "pondjs"
-import { Charts, ChartContainer, ChartRow, YAxis, LineChart,Resizable, styler, Legend, TimeMarker } from "react-timeseries-charts"
+import { Charts, ChartContainer, ChartRow, YAxis, LineChart, styler } from "react-timeseries-charts"
 import bp from '../constants/breakpoints'
 import Table from './Table'
 import { API_LINK, API_NAME } from '../constants/api'
 import {default as $} from 'jquery'
 
+// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2018-08-01 - last 2018-11-21
 type tPROPS = {
     accessSinceLaunch: string,
     dynamicDisclaimer: string
