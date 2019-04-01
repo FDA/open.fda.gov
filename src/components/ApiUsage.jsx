4aa2922544bf6ff3124ea04e03acc59693a8592f
beardedfinch <beardedfinch@gmail.com>
2019-04-01 10:06:13 -0500

    Usage numbers update.

--- src/components/ApiUsage.jsx
+++ src/components/ApiUsage.jsx
@@ -6,7 +6,8 @@ import Table from './Table'
 import { API_LINK, API_NAME } from '../constants/api'
 import {default as $} from 'jquery'
 
-// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2019-02-21 - last 2019-02-21
+// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2019-03-21 - last 2019-03-21
+// Update in pages/about/statistics/_content.yaml
 type tPROPS = {
     accessSinceLaunch: string,
     dynamicDisclaimer: string
