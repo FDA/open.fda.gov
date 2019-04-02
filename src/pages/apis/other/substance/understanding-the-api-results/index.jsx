950519464f0ddd14ec2eb725f3ad5ec8a2ed433d
beardedfinch <beardedfinch@gmail.com>
2019-04-02 12:39:27 -0500

    Added fields and fixed demo link.

--- src/pages/apis/other/substance/understanding-the-api-results/index.jsx
+++ src/pages/apis/other/substance/understanding-the-api-results/index.jsx
@@ -10,14 +10,14 @@ class IndexRoute extends React.Component {
     return (
       <section className="doc-content">
         <h2>Understanding the API Results</h2>
-        <p>For <code>search</code> queries (such as: <a href='https://api.fda.gov/other/substance.json?search=definition_type:"PRIMARY"'>https://api.fda.gov/other/substance.json?search=definition_type:"PRIMARY"</a>), the <code>results</code> section includes matching adverse event reports returned by the API.</p>
+        <p>For <code>search</code> queries (such as: <a href='http://54.196.38.238:8000/other/substance.json?search=definition_type:"PRIMARY"'>http://54.196.38.238:8000/other/substance.json?search=definition_type:"PRIMARY"</a>), the <code>results</code> section includes matching adverse event reports returned by the API.</p>
         <p>Each nsde result consists of these major sections:</p>
         <ul>
           <li><code>References:</code>These fields relate to the product itself.</li>
           <li><code>Relationships:</code>Information about the marketing of the product.</li>
           <li><code>Names:</code>Information about the marketing of the product.</li>
         </ul>
-        <p>For count queries (such as: <a href="https://api.fda.gov/other/substance.json?count=status">https://api.fda.gov/other/substance.json?count=status</a>), the results section will look something like the following:</p>
+        <p>For count queries (such as: <a href="http://54.196.38.238:8000/other/substance.json?count=status">http://54.196.38.238:8000/other/substance.json?count=status</a>), the results section will look something like the following:</p>
         <Highlight
           className='javascript'>
           {example}
