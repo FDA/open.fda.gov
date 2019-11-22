2923a81a695851eaa5e2f60189d399ae6655450f
Becky Conning <becky.conning@icloud.com>
2019-11-22 19:09:31 +0000

    Update index.jsx

--- src/pages/apis/authentication/index.jsx
+++ src/pages/apis/authentication/index.jsx
@@ -21,7 +21,7 @@ class IndexRoute extends React.Component {
         <p><code>https://api_basics.fda.gov/drug/event.json?api_key=yourAPIKeyHere&search=...</code></p>
         <h3>HTTPS requests only</h3>
         <p>Alternatively your API key may be provided as a basic auth username. For example:</p>
-        <p><code>Authorization: Basic yourAPIKeyHere</code></p>
+        <p><code>Authorization: Basic eW91ckFQSUtleUhlcmU6</code></p>
         <p>OpenFDA requires you to use <code>https://api_basics.fda.gov</code> for all queries to ensure secure communication.</p>
       </section>
     )
