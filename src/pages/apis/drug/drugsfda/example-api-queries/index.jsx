4c57d5a97e16f4dc220f0f3a3e680035d481576a
beardedfinch <beardedfinch@gmail.com>
2020-08-26 11:20:10 -0500

    Final changes.

--- src/pages/apis/drug/drugsfda/example-api-queries/index.jsx
+++ src/pages/apis/drug/drugsfda/example-api-queries/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import QueryExplorer from '../../../../../components/QueryExplorer'
+import QueryTour from '../../../../../components/QueryTour'
 import explorers from '../_explorers.yaml'
 
 class IndexRoute extends React.Component {
@@ -14,23 +14,26 @@ class IndexRoute extends React.Component {
       <section className="doc-content">
         <h2>Example Drugs@FDA queries</h2>
         <p>To help get you started, we have provided some query examples below. Use the Run query button to call the Application Programming Interface and get back results. You can experiment by editing the example queries in the black text box.</p>
-        <QueryExplorer
+        <QueryTour
           desc={oneApplication.description}
-          originalQuery={oneApplication.query}
+          query={oneApplication.query}
           params={oneApplication.params}
           title={oneApplication.title}
+          name={'oneApplication'}
         />
-        <QueryExplorer
+        <QueryTour
           desc={lotion.description}
-          originalQuery={lotion.query}
+          query={lotion.query}
           params={lotion.params}
           title={lotion.title}
+          name={'lotion'}
         />
-        <QueryExplorer
+        <QueryTour
           desc={sponsorNameCount.description}
-          originalQuery={sponsorNameCount.query}
+          query={sponsorNameCount.query}
           params={sponsorNameCount.params}
           title={sponsorNameCount.title}
+          name={'sponsorNameCount'}
         />
       </section>
     )
