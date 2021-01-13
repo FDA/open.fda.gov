b3b964a54e7fd3a1379ed17b89c3ba3065ead841
Denis Krylov <dkrylovsb@users.noreply.github.com>
2021-01-13 11:51:04 -0500

    Cypress integration and the first test for the home page.

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+context('Homepage', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Page title should be openFDA', () => {
+        cy.title().should('eq', 'openFDA')
+    })
+
+    it('Disclaimer should automatically show up', () => {
+        cy.get('.modal-container')
+            .should('be.visible').within(() => {
+            cy.get('.modal-header').should('be.visible').should('have.text', 'Disclaimer')
+            cy.get('.modal-body').should('be.visible')
+            cy.get('button.bg-primary').should('be.visible').should('have.text', 'ACCEPT')
+        }).find('button.bg-primary').click();
+
+        cy.get('.modal-container').should('not.exist');
+    })
+
+})
