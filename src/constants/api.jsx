596267bb4c7542fa9fdec993a03c5595747d2d4c
beardedfinch <beardedfinch@gmail.com>
2021-03-05 08:51:02 -0600

    Fix to API constants.

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,5 +1,5 @@
-const API_LINK: string = "https://openfda-api.preprod.fda.gov/drug/orange_book.json"
-const API_NAME: string = "api.fda.gov"
+export const API_LINK: string = "https://openfda-api.preprod.fda.gov"
+export const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
   API_NAME
