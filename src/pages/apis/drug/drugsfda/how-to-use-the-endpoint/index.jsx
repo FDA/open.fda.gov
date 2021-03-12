30a2e57e1e10427797fb5572338052321b4064f4
Denis Krylov <dkrylovsb@users.noreply.github.com>
2021-03-12 11:29:52 -0500

    Added Drug NDC tests.

--- src/pages/apis/drug/drugsfda/how-to-use-the-endpoint/index.jsx
+++ src/pages/apis/drug/drugsfda/how-to-use-the-endpoint/index.jsx
@@ -10,7 +10,7 @@ class IndexRoute extends React.Component {
 
     return (
       <section className='doc-content'>
-        <h2>How to use the Endpoint</h2>
+        <h2>How to use the API</h2>
         <p>Getting started with and learning how to use the Application Programming Interface is relatively straightforward. Here are some recommended steps:</p>
         <APIUseSteps endpoint_name='Drugs@FDA' endpoint_path='/apis/drug/drugsfda/'/>
         <p>Then, when you are ready, obtain an Application Programming Interface Key. While you don’t need an Application Programming Interface Key to use the Application Programming Interface, we recommend you get one if you are planning to use the Application Programming Interface on a regular basis. For more information on Application Programming Interface Keys, see the <Link to="/apis/authentication/">Authentication</Link> documentation.</p>
