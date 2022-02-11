f8a8d797e343e78f3f7cee3956115c97697a79fa
beardedfinch <beardedfinch@gmail.com>
2022-02-11 09:45:00 -0600

    Updated usage numbers

--- src/components/ApiUsage.jsx
+++ src/components/ApiUsage.jsx
@@ -7,7 +7,7 @@ import { API_LINK, API_NAME } from '../constants/api'
 import {default as $} from 'jquery'
 import '../css/components/APIUsage.scss'
 
-// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2022-01-11 - last 2022-01-11
+// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2022-02-11 - last 2022-02-11
 // Update in pages/about/statistics/_content.yaml
 type tPROPS = {
     accessSinceLaunch: string,
