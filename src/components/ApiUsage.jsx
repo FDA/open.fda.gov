16ccce32ebec2fe31b131a91d0c6359bce4ab613
violet.wren <violet.wren@semanticbits.com>
2023-05-03 09:35:23 -0500

    API Usage Update May 2023

--- src/components/ApiUsage.jsx
+++ src/components/ApiUsage.jsx
@@ -7,7 +7,7 @@ import { API_LINK, API_NAME } from '../constants/api'
 import {default as $} from 'jquery'
 import '../css/components/APIUsage.scss'
 
-// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2023-04-03 - last 2023-04-03
+// Update total usage numbers with: https://api.fda.gov/usage.json?end_at=2023-05-03 - last 2023-05-03
 // Update in pages/about/statistics/_content.yaml
 type tPROPS = {
     accessSinceLaunch: string,
