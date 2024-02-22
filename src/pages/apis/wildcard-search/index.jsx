9f0a8610d0cc1d6ed687d791c6aeff5d063fe451
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-02-22 12:10:27 -0500

    Updated wording and added more query examples to the `Wildcard search` page.

--- src/pages/apis/wildcard-search/index.jsx
+++ src/pages/apis/wildcard-search/index.jsx
@@ -8,6 +8,8 @@ class IndexRoute extends React.Component {
 
     const wildcardSearchExactField = explorers['wildcardSearchExactField']
     const wildcardSearchRegularField = explorers['wildcardSearchRegularField']
+    const wildcardSearchFoodRecallInsensitive = explorers['wildcardSearchFoodRecallInsensitive']
+    const wildcardSearchFoodRecallSensitive = explorers['wildcardSearchFoodRecallSensitive']
 
 
     return (
@@ -16,6 +18,7 @@ class IndexRoute extends React.Component {
         <p>Wildcard queries return documents that contain terms matching a wildcard pattern.</p>
         <p>A wildcard operator is a placeholder that matches one or more characters. At this point, openFDA supports the <code>*</code> ("star") wildcard operator, which
             matches zero or more characters. You can combine wildcard operators with other characters to create a wildcard pattern.</p>
+        <p>This feature is available on all API Endpoints.</p>
         <p>Here are some example queries that demonstrate how wildcard searches work.</p>
 
         <QueryTour
@@ -32,6 +35,20 @@ class IndexRoute extends React.Component {
               title={wildcardSearchExactField.title}
               name={'wildcardSearchExactField'}
           />
+      <QueryTour
+          desc={wildcardSearchFoodRecallInsensitive.description}
+          query={wildcardSearchFoodRecallInsensitive.query}
+          params={wildcardSearchFoodRecallInsensitive.params}
+          title={wildcardSearchFoodRecallInsensitive.title}
+          name={'wildcardSearchFoodRecallInsensitive'}
+      />
+      <QueryTour
+          desc={wildcardSearchFoodRecallSensitive.description}
+          query={wildcardSearchFoodRecallSensitive.query}
+          params={wildcardSearchFoodRecallSensitive.params}
+          title={wildcardSearchFoodRecallSensitive.title}
+          name={'wildcardSearchFoodRecallSensitive'}
+      />
       </section>
     )
   }
