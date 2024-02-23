303755c37f6450b51c35b29099f2c4212607e616
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-02-23 12:08:18 -0500

    Fix/wildcard search wording (#253)
    * Updated wording and added more query examples to the `Wildcard search` page.
    
    * bold font to this statement on the page:  “This feature is available on all API Endpoints.”
    “Wildcard queries return documents that contain terms matching a wildcard pattern.”   “data” should replace “documents” in that statement.
    
    * Changes to the wording

--- src/pages/apis/wildcard-search/index.jsx
+++ src/pages/apis/wildcard-search/index.jsx
@@ -8,14 +8,17 @@ class IndexRoute extends React.Component {
 
     const wildcardSearchExactField = explorers['wildcardSearchExactField']
     const wildcardSearchRegularField = explorers['wildcardSearchRegularField']
+    const wildcardSearchFoodRecallInsensitive = explorers['wildcardSearchFoodRecallInsensitive']
+    const wildcardSearchFoodRecallSensitive = explorers['wildcardSearchFoodRecallSensitive']
 
 
     return (
       <section className="doc-content">
         <h2>Wildcard search</h2>
-        <p>Wildcard queries return documents that contain terms matching a wildcard pattern.</p>
+        <p>Wildcard queries return data that contain terms matching a wildcard pattern.</p>
         <p>A wildcard operator is a placeholder that matches one or more characters. At this point, openFDA supports the <code>*</code> ("star") wildcard operator, which
             matches zero or more characters. You can combine wildcard operators with other characters to create a wildcard pattern.</p>
+        <p><strong>This feature is available on all API Endpoints.</strong></p>
         <p>Here are some example queries that demonstrate how wildcard searches work.</p>
 
         <QueryTour
@@ -32,6 +35,20 @@ class IndexRoute extends React.Component {
               title={wildcardSearchExactField.title}
               name={'wildcardSearchExactField'}
           />
+      <QueryTour
+          desc={wildcardSearchFoodRecallInsensitive.description}
+          query={wildcardSearchFoodRecallInsensitive.query}
+          params={wildcardSearchFoodRecallInsensitive.params}
+          title={wildcardSearchFoodRecallInsensitive.title}
+          name={'wildcardSearchFoodRecallInsensitive'}
+      />
+      <QueryTour
+          desc={wildcardSearchFoodRecallSensitive.description}
+          query={wildcardSearchFoodRecallSensitive.query}
+          params={wildcardSearchFoodRecallSensitive.params}
+          title={wildcardSearchFoodRecallSensitive.title}
+          name={'wildcardSearchFoodRecallSensitive'}
+      />
       </section>
     )
   }
