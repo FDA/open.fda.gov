61b35192273169821b34d899345e4f402f3b326a
violet.wren <violet.wren@semanticbits.com>
2025-03-03 09:56:06 -0800

    Usage updates - Feb & March 2025

--- src/components/ApiUsage.jsx
+++ src/components/ApiUsage.jsx
@@ -7,7 +7,7 @@ import { API_LINK, API_NAME } from '../constants/api'
 import {default as $} from 'jquery'
 import '../css/components/APIUsage.scss'
 
-// Update total usage numbers with: https://api.fda.gov/usage.json?start_at=2025-01-06 - last 2025-01-06
+// Update total usage numbers with: https://api.fda.gov/usage.json?start_at=2025-03-03 - last 2025-03-03
 
 // Update in pages/about/statistics/_content.yaml
 type tPROPS = {
