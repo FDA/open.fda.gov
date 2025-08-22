87c6fd415e2a3d78840f0f4a396920ecc8922d07
violet.wren <violet.wren@semanticbits.com>
2025-08-22 08:21:01 -0700

    Fixed abbreviation

--- src/pages/apis/transparency/completeresponseletters/index.tsx
+++ src/pages/apis/transparency/completeresponseletters/index.tsx
@@ -8,7 +8,7 @@ class IndexRoute extends React.Component {
     return (
       <section className='doc-content'>
         <h2>Complete Response Letters</h2>
-        <p>These records include Complete Response Letters (CRL) issued in response to new drug applications
+        <p>These records include Complete Response Letters (CRLs) issued in response to new drug applications
         (NDAs) and biologics license applications (BLAs). This is the first ever centralized database of past CRLs.</p>
         <a href='https://download.open.fda.gov/ApprovedCRLs_NDA_BLA_2020-2024.zip'>Download Complete Response Letters in PDF format</a>
         <KeyFacts
