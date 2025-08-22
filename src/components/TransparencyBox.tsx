46c67c5c67a1fc4df237ab13f5f57d7a8e322045
violet.wren <violet.wren@semanticbits.com>
2025-08-22 12:15:35 -0700

    Updated Transparency Tools

--- src/components/TransparencyBox.tsx
+++ src/components/TransparencyBox.tsx
@@ -52,13 +52,13 @@ const TransparencyBox = (props: tPROPS) => {
 
   const ep_path: Record<NounName, Partial<Record<EndpointName, string>>> = {
     'transparency': {
-      'crl': '/apis/transparency/completeresponseletters/'
+      'crl': 'https://openfda-site.preprod.fda.gov/apis/transparency/completeresponseletters/'
     }
   }
 
   return (
     <section id='endpoint_box' className='marg-2 endpoint-card'>
-      <Link className='ep-box' to={ep_path[noun_name][endpoint_name] ?? ''}>
+      <a className='ep-box' href={ep_path[noun_name][endpoint_name]}>
         <div className='ep-icon-container'>
           {icon[noun_name][endpoint_name]}
         </div>
@@ -67,10 +67,10 @@ const TransparencyBox = (props: tPROPS) => {
           <span className='marg-1 clr-black'>{description[noun_name][endpoint_name]}</span>
         </div>
         <span className='btn btn-icon-right clr-primary weight-700'>LEARN MORE<i className='fa fa-angle-right marg-l-1'/></span>
-      </Link>
+      </a>
     </section>
   )
 }
 
-EndpointBox.displayName = 'components/TransparencyBox'
+TransparencyBox.displayName = 'components/TransparencyBox'
 export default TransparencyBox
