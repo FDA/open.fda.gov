62feb7112ea0ec94e6b6f40c8a597c7d2226153a
Denis Krylov <dkrylovsb@users.noreply.github.com>
2025-09-04 08:31:52 -0400

    Updated CRL download link to the latest version

--- src/pages/apis/transparency/completeresponseletters/index.tsx
+++ src/pages/apis/transparency/completeresponseletters/index.tsx
@@ -10,8 +10,7 @@ class IndexRoute extends React.Component {
         <h2>Complete Response Letters</h2>
         <p>These records include Complete Response Letters (CRLs) issued in response to new drug applications
         (NDAs) and biologics license applications (BLAs). This is the first ever centralized database of past CRLs.</p>
-        <p><a href='https://download.open.fda.gov/CRL_Batch_2_09_03_2025.zip'>Download Complete Response Letters in PDF format</a></p>
-        <p><a href='https://open.fda.gov/crltable/'>Search for CRLs</a></p>
+        <p><a href='https://download.open.fda.gov/CRLs.zip'>Download Complete Response Letters in PDF format</a></p>
         <KeyFacts
           noun_name={meta.api_path.split("/")[1]}
           endpoint_name={meta.api_path.split("/")[2]}
