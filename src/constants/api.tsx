6a1e4ccdee0d7a14a7fba6b6061ad687579adb48
Denis Krylov <dkrylovsb@users.noreply.github.com>
2025-09-11 12:49:35 -0400

    Revert "Revert "Update API link to production in api.tsx and _explorers.yaml""
    This reverts commit 1fd536895c46cae87bfe1566b75f439b915e492b.

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,5 +1,4 @@
-export const API_LINK: string = "https://openfda-api.preprod.fda.gov"
-//export const API_LINK: string = "https://api.fda.gov"
+export const API_LINK: string = "https://api.fda.gov"
 export const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
