4ffeb26cde8a2db4baa352e15d86027398af6c41
Violet Wren <violet.wren@semanticbits.com>
2025-10-01 12:26:10 -0700

    Reglist bugfix (#295)
    * Bug fixes for reglist
    
    * Additional bug fixes for reglist

--- src/pages/apis/device/registrationlisting/understanding-the-api-results/index.tsx
+++ src/pages/apis/device/registrationlisting/understanding-the-api-results/index.tsx
@@ -11,12 +11,12 @@ class IndexRoute extends React.Component {
     return (
       <section className='doc-content'>
         <h2>Understanding the API Results</h2>
-        <p>For <code>search</code> queries (such as: <a href='https://api.fda.gov/device/recall.json?search=product_code:FOZ'>https://api.fda.gov/device/recall.json?search=product_code:FOZ</a>), the <code>results</code> section includes matching Device Recall reports returned by the API.</p>
+        <p>For <code>search</code> queries (such as: <a href='https://api.fda.gov/device/registrationlisting.json'>https://api.fda.gov/device/registrationlisting.json</a>), the <code>results</code> section includes matching Device Registration & Listings reports returned by the API.</p>
         <Highlight
           className='javascript'>
           {anatomy}
         </Highlight>
-        <p>For <code>count</code> queries (such as: <a href='https://api.fda.gov/device/recall.json?count=product_code'>https://api.fda.gov/device/recall.json?count=product_code</a>), the results section will look something like the following:</p>
+        <p>For <code>count</code> queries (such as: <a href='https://api.fda.gov/device/registrationlisting.json?count=establishment_type.exact'>https://api.fda.gov/device/registrationlisting.json?count=establishment_type.exact</a>), the results section will look something like the following:</p>
         <Highlight
           className='javascript'>
           {count}
