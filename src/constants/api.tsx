5cecce249e3a7f9ca02d008210f33316810f1c24
Denis Krylov <dkrylovsb@users.noreply.github.com>
2025-12-03 10:40:52 -0500

    Update API endpoint to use OpenFDA preproduction server

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,4 +1,4 @@
-export const API_LINK: string = "http://44.205.221.207:8000"
+export const API_LINK: string = "https://openfda-api.preprod.fda.gov"
 export const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
