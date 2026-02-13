fa5fbbd31a54f00b884b742ce40d4ce36f6fcbe2
Denis Krylov <dkrylovsb@users.noreply.github.com>
2026-02-13 14:58:44 -0500

    Revert "Add sidebar visibility for tobacco research API routes"
    This reverts commit f72c7c27eed1db4db4207bff59738959ef20daec.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -37,7 +37,7 @@ const Layout = (props: LayoutProps) => {
     up: 'sticky-scroll-up'
   }
 
-  const hasSidebar = props.location.pathname.slice(0, 6) === `/apis/` || props.location.pathname.slice(0, 23) === `/tobacco-research/apis/`
+  const hasSidebar = props.location.pathname.slice(0, 6) === `/apis/`
   return (
     <DocumentTitle title='openFDA' key='openFDA'>
       <div>
