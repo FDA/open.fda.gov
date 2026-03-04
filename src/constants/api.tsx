e0066edb86b6da38082e919784d77e5c96be66a0
violet.wren <violet.wren@semanticbits.com>
2026-03-04 09:50:45 -0800

    Enabled path prefix

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,4 +1,4 @@
-export const API_LINK: string = "http://34.199.139.221:8000"
+export const API_LINK: string = "https://openfda-api.preprod.fda.gov"
 export const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
