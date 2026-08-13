4fb6759d6a240709da3d63e379b8881b19c63106
violet.wren <violet.wren@semanticbits.com>
2026-08-13 11:39:26 -0700

    Updated description

--- src/pages/apis/research/covidmirnaandproteomics/index.tsx
+++ src/pages/apis/research/covidmirnaandproteomics/index.tsx
@@ -11,7 +11,7 @@ class IndexRoute extends React.Component {
       <section className='doc-content'>
         <h2>Covid miRNA and Proteomics Research Overview</h2>
 
-        <p>This dataset makes available the research paper "Integrated miR-omics and Proteomics Reveal the Regulatory Role of miR in Protein Networks Associated with COVID-19 Disease Progression"</p>
+        <p>This dataset is associated with the research paper "Integrated miR-omics and Proteomics Reveal the Regulatory Role of miR in Protein Networks Associated with COVID-19 Disease Progression"</p>
 
         <h3>Responsible use of the data</h3>
         <p>Do not rely on openFDA to make decisions regarding medical care. Always speak to your health provider about the risks and benefits of FDA-regulated products. We may limit or otherwise restrict your access to the API in line with our <Link to='/terms/'>Terms of Service</Link>.</p>
