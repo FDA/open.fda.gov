692078621008afe5dd2be8fae20aef04bb008245
violet.wren <violet.wren@semanticbits.com>
2026-08-25 13:40:01 -0700

    Added COVID download files.

--- src/pages/apis/research/covidmirnaandproteomics/index.tsx
+++ src/pages/apis/research/covidmirnaandproteomics/index.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import Link from "gatsby-link"
 
-import KeyFacts from '../../../../components/RenderContentObject/KeyFacts'
-import meta from './_meta.yaml'
 
 class IndexRoute extends React.Component {
   render () {
@@ -12,6 +10,8 @@ class IndexRoute extends React.Component {
         <h2>Covid miRNA and Proteomics Research Overview</h2>
 
         <p>This dataset is associated with the research paper "Integrated miR-omics and Proteomics Reveal the Regulatory Role of miR in Protein Networks Associated with COVID-19 Disease Progression"</p>
+
+        <p><a href='https://download.open.fda.gov/covid_mirna_and_proteomics_source_data.zip'>Download the source files</a></p>
 
         <h3>Responsible use of the data</h3>
         <p>Do not rely on openFDA to make decisions regarding medical care. Always speak to your health provider about the risks and benefits of FDA-regulated products. We may limit or otherwise restrict your access to the API in line with our <Link to='/terms/'>Terms of Service</Link>.</p>
