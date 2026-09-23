c615d8f07b6bf60c56eae1098167aad5eb853e61
violet.wren <violet.wren@semanticbits.com>
2026-09-23 12:27:48 -0700

    Updated to preprod link

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,5 +1,5 @@
 // export const API_LINK: string = "https://api.fda.gov"
-export const API_LINK: string = "http://18.233.57.46:8000"
+export const API_LINK: string = "http://openfda-api.preprod.fda.gov:8000"
 export const API_NAME: string = "api.fda.gov"
 export default {
   API_LINK,
